935728730cf33e1ef4770703e7bb8846fc4a10d9
sunshine-agoo <sun.molina@gmail.com>
2024-03-05 17:20:57 -0500

    Added video details in the Hero section

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -3,9 +3,32 @@ import "./Hero.scss";
 import playButton from "../../assets/images/icons/play.svg";
 import fullscreenButton from "../../assets/images/icons/fullscreen.svg";
 import volumeUpButton from "../../assets/images/icons/volume_up.svg";
+import viewsIcon from "../../assets/images/icons/views.svg";
+import likesIcon from "../../assets/images/icons/likes.svg";
 
 
-const Hero = ( {selectedVideo}) => {
+const Hero = ( {selectedVideo} ) => {
+    // Format timestamp
+    const formatTimestamp = (timestamp) => {
+        try {
+            const formattedDate = new Date(timestamp);
+            const dateOptions = { 
+                month: "2-digit",
+                day: "numeric",
+                year: "numeric",
+            };
+            return formattedDate.toLocaleDateString("en-US", dateOptions);
+        } catch (error) {
+            console.error("Error formatting timestamp:", error);
+            return timestamp;
+        }
+    };
+
+    const formattedTimestamp = formatTimestamp(selectedVideo.timestamp);
+
+    // Selected video's number of comments
+    const numOfComments = selectedVideo.comments.length;
+
     return (
         <section className="hero">
             {/* Render Main Video */}
@@ -35,9 +58,38 @@ const Hero = ( {selectedVideo}) => {
                     alt={selectedVideo.title}
                 />
             </div>
+            {/* Video Details */}
             <div className="hero__video-details-container">
                 <div className="hero__video-title-container">
                     <h1 className="hero__video-title">{selectedVideo.title}</h1>
+                </div>
+                <div className="hero__video-info-divider-container--top">
+                    <hr className="hero__video-info-divider--top" />
+                </div>
+                <div className="hero__video-info--top">
+                    <div className="hero__video-info--left">
+                        <p className="hero__channel">By {selectedVideo.channel}</p>
+                        <p className="hero__timestamp">{formattedTimestamp}</p>
+                    </div>
+                    <div className="hero__video-info--right">
+                        <div className="hero__video-views-container">
+                            <img src={viewsIcon} alt="Views Icon" className="hero__views-icon"/>
+                            <p className="hero__views">{selectedVideo.views}</p>
+                        </div>
+                        <div className="hero__video-likes-container">
+                            <img src={likesIcon} alt="Likes Icon" className="hero__likes-icon"/>
+                            <p className="hero__likes">{selectedVideo.likes}</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="hero__video-info-divider-container--bottom">
+                    <hr className="hero__video-info-divider--bottom" />
+                </div>    
+                <div className="hero__video-info--bottom">
+                    <p className="hero__description">{selectedVideo.description}</p>
+                </div>
+                <div className="hero__video-comments-counter">
+                    <p className="hero__comments-counter">{numOfComments} Comments</p>
                 </div>
             </div>
         </section>
