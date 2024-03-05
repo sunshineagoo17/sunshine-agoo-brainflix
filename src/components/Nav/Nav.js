bb99ffaa7e1517ae0d9dc8d58d54d69c6f5dd560
sunshine-agoo <sun.molina@gmail.com>
2024-03-04 23:21:04 -0500

    Finished tablet and desktop versions of Nav

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -6,11 +6,11 @@ import UploadIcon from "../../assets/images/icons/upload.svg";
 import avatarImg from "../../assets/images/Mohan-muruge.jpg";
 
 const Nav = () => {
-    const [isTyping, setIsTyping] = useState(false);
+    const [userIsTyping, updateTypingStatus] = useState(false);
 
-    const handleInputChange = event => {
+    const handleSearchInputChange = event => {
         const inputValue = event.target.value;
-        setIsTyping(inputValue.length > 0);
+        updateTypingStatus(inputValue.length > 0);
     };
 
     return (
@@ -18,18 +18,30 @@ const Nav = () => {
             <img src={BrainFlixLogo} alt="BrainFlix logo" className="nav__logo" />
             <div className="nav__search-list">
                 <div className="nav__search-container">
-                    {!isTyping && <img src={SearchIcon} alt="search icon" className="nav__search-icon" />}
-                    <input type="text" placeholder="Search" className="nav__search-bar" onChange={handleInputChange}/>
+                    {!userIsTyping && <img src={SearchIcon} alt="search icon" className="nav__search-icon" />}
+                    <input type="text" placeholder="Search" className="nav__search-bar" onChange={handleSearchInputChange}/>
+                </div>
+                <div className="nav__upload-button-container--left">
+                <button className="nav__upload-button--left">
+                    <div className="nav__upload-icon-container">
+                        <img src={UploadIcon} alt="upload icon" className="nav__upload-icon" />
+                    </div>
+                    <div className="nav__spacer" />
+                    <div className="nav__upload-copy">
+                        Upload
+                    </div>
+                </button>
                 </div>
                 <div className="nav__avatar-container">
                     <img src={avatarImg} className="nav__avatar" alt="Mohan Muruge" />
                 </div>
             </div>
-            <div className="nav__upload-button-container">
-                <button className="nav__upload-button">
+            <div className="nav__upload-button-container--right">
+                <button className="nav__upload-button--right">
                     <div className="nav__upload-icon-container">
                         <img src={UploadIcon} alt="upload icon" className="nav__upload-icon" />
                     </div>
+                    <div className="nav__spacer" />
                     <div className="nav__upload-copy">
                         Upload
                     </div>
