bb56ba822b100b00bdaf6ecbfb81905000e9ceea
sunshine-agoo <sun.molina@gmail.com>
2024-03-20 17:00:12 -0400

    Updated Video Info component name and classnames

--- src/components/VideoInfo/VideoInfo.js
+++ src/components/VideoInfo/VideoInfo.js
@@ -1,4 +1,4 @@
-import { TimeAgo } from "../../App";
+import { TimeAgo } from "../VideoPageManager/VideoPageManager";
 
 // Imports the stylesheet for the VideoDetails component
 import "./VideoInfo.scss";
@@ -7,56 +7,59 @@ import "./VideoInfo.scss";
 import ViewsIcon from "../../assets/images/icons/views.svg";
 import LikesIcon from "../../assets/images/icons/likes.svg";
 
-const VideoDetails = ({ mainVideo }) => {
+const VideoInfo = ({ mainVideo }) => {
+    // Checks if there's a video to display, otherwise stops the function early
+    if (!mainVideo) return null;
+
     // Format timestamp with TimeAgo function
     const formattedTimestamp = TimeAgo(mainVideo.timestamp);
 
-    // Get the number of comments for the selected video
-    const numOfComments = mainVideo.comments.length;
+    // Calculates the number of comments. If there are no comments, it defaults to 0
+    const numOfComments = mainVideo.comments ? mainVideo.comments.lenth : 0;
 
     return (
-        <section className="videoDetails">
+        <section className="videoInfo">
             {/* Display video title */}
-            <div className="videoDetails__video-title-container">
-                <h1 className="videoDetails__video-title">{mainVideo.title}</h1>
+            <div className="videoInfo__video-title-container">
+                <h1 className="videoInfo__video-title">{mainVideo.title}</h1>
             </div>
             {/* Divider above video info */}
-            <div className="videoDetails__video-info-divider-container--top">
-                <hr className="videoDetails__video-info-divider--top" />
+            <div className="videoInfo__video-details-divider-container--top">
+                <hr className="videoInfo__video-details-divider--top" />
             </div>
             {/* Video info section */}
-            <div className="videoDetails__video-info--top">
+            <div className="videoInfo__video-details--top">
                 {/* Channel and Timestamp */}
-                <div className="videoDetails__video-info--left">
-                    <p className="videoDetails__channel">By {mainVideo.channel}</p>
-                    <p className="videoDetails__timestamp">{formattedTimestamp}</p>
+                <div className="videoInfo__video-details--left">
+                    <p className="videoInfo__channel">By {mainVideo.channel}</p>
+                    <p className="videoInfo__timestamp">{formattedTimestamp}</p>
                 </div>
                 {/* Views and Likes */}
-                <div className="videoDetails__video-info--right">
-                    <div className="videoDetails__video-views-container">
-                        <img src={ViewsIcon} alt="Views Icon" className="videoDetails__views-icon"/>
-                        <p className="videoDetails__views">{mainVideo.views}</p>
+                <div className="videoInfo__video-info--right">
+                    <div className="videoInfo__video-views-container">
+                        <img src={ViewsIcon} alt="Views Icon" className="videoInfo__views-icon"/>
+                        <p className="videoInfo__views">{mainVideo.views}</p>
                     </div>
-                    <div className="videoDetails__video-likes-container">
-                        <img src={LikesIcon} alt="Likes Icon" className="videoDetails__likes-icon"/>
-                        <p className="videoDetails__likes">{mainVideo.likes}</p>
+                    <div className="videoInfo__video-likes-container">
+                        <img src={LikesIcon} alt="Likes Icon" className="videoInfo__likes-icon"/>
+                        <p className="videoInfo__likes">{mainVideo.likes}</p>
                     </div>
                 </div>
             </div>
             {/* Divider below video info */}
-            <div className="videoDetails__video-info-divider-container--bottom">
-                <hr className="videoDetails__video-info-divider--bottom" />
+            <div className="videoInfo__video-details-divider-container--bottom">
+                <hr className="videoInfo__video-details-divider--bottom" />
             </div>    
             {/* Video description */}
-            <div className="videoDetails__video-info--bottom">
-                <p className="videoDetails__description">{mainVideo.description}</p>
+            <div className="videoInfo__video-details--bottom">
+                <p className="videoInfo__description">{mainVideo.description}</p>
             </div>
             {/* Displays the number of comments */}
-            <div className="videoDetails__video-comments-counter">
-                <p className="videoDetails__comments-counter">{numOfComments} Comments</p>
+            <div className="videoInfo__video-comments-counter">
+                <p className="videoInfo__comments-counter">{numOfComments} Comments</p>
             </div>
         </section>
     );
 };
 
-export default VideoDetails;
+export default VideoInfo;
