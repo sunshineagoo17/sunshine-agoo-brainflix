aed44397f551e1bc00962c6caf7fddb95eee3480
sunshine-agoo <sun.molina@gmail.com>
2024-03-20 14:45:14 -0400

    Styled Video Upload page

--- src/App.js
+++ src/App.js
@@ -1,63 +1,41 @@
-import React, { useState } from "react";
+import React from "react";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 // Imports necessary components that support the App component
 import Header from "./components/Header/Header";
-import Hero from './components/Hero/Hero';
-import VideoInfo from "./components/VideoInfo/VideoInfo";
-import Comments from "./components/Comments/Comments";
-import SideVideos from './components/SideVideos/SideVideos';
+import VideoPageManager from "./components/VideoPageManager/VideoPageManager";
 
-// Imports video data from the JSON file
-import VideoData from "./data/video-details.json";
+// Imports page components
+import VideoUploadPage from "./pages/VideoUploadPage/VideoUploadPage";
+import VideoDetailsPage from "./pages/VideoDetailsPage/VideoDetailsPage";
+import Homepage from "./pages/Homepage/Homepage";
+import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
 
 // Imports the main stylesheet for the App component
 import './App.scss';
 
 const App = () => {
-  // State to keep track of the list of videos
-  const [videos, setVideos] = useState(VideoData);
-  // State to track the selected video
-  const [mainVideoId, setMainVideoId] = useState(VideoData[0].id);
-
-  // Find the main video details using mainVideoId
-  const mainVideoDetails = videos.find(video => video.id === mainVideoId);
-
-  const handleVideoSelect = (selectedVideoId) => {
-    setMainVideoId(selectedVideoId);
-
-    // Updates the video list, setting the selected video as the main and filtering out its previous instance  
-    setVideos(initialVideos => {
-      const newMainVideo = initialVideos.find(video => video.id === selectedVideoId);
-      const filteredVideos = initialVideos.filter(video => video.id !== selectedVideoId);
-      return [newMainVideo, ...filteredVideos];
-    });
-  };
-
-  // JSX for rendering the App component
+  // Render method for App component - defines routing and layout structure
   return (
-    <div className="app">
-      {/* Displays Header component */}
-      <Header />
-      {/* Displays Hero component */}
-      <Hero mainVideo={mainVideoInfo} />
-      <div className="app__video-details-container">
-        <div className="app__video-details-text">
-          {/* Displays main video description */}
-          <VideoInfo mainVideo={mainVideoDetails} />
-          {/* Displays comments for the selected video */}
-          <Comments comments={mainVideoDetails.comments} />
-        </div>
-        <div className="app__video-details-thumbnails-container">
-          <div className="app__divider-container">
-              <hr className="app__divider" />
-          </div>
-          <div className="app__video-details-thumbnails">
-            {/* Displays a separate list of video thumbails */}
-            <SideVideos videos={videos.filter(video => video.id !== mainVideoId)} handleVideoClick={handleVideoSelect} />
-          </div>
-        </div>
+    <Router>
+      <div className="app">
+        {/* Header component is displayed across all routes for consistent navigation */}
+        <Header />
+        <Routes>
+          {/* Redirect route path "/" to home */}
+          <Route path="/" element={<Navigate to="/home" />} />
+          {/* Homepage and VideoDetailsPage are wrapped within VideoPageManager for context provision */}
+          <Route path="/home" element={<VideoPageManager><Homepage /></VideoPageManager>} />
+          <Route path="/video/:videoId" element={<VideoPageManager><VideoDetailsPage /></VideoPageManager>} />
+          {/* VideoUploadPage is standalone and doesn't require context from VideoManager */}
+          <Route path="/upload" element={<VideoUploadPage />} />
+          {/* Redirect 404 path */}
+          <Route path="*" element={<Navigate to="/uh-oh" />} />
+          {/* Handle 404 errors */}
+          <Route path="/uh-oh" element={<NotFoundPage />} />
+        </Routes>
       </div>
-    </div>
+    </Router>
   );
 }
 
