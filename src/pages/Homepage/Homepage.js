1f155b4023cbeaff13c1227e95007605811506bd
sunshine-agoo <sun.molina@gmail.com>
2024-03-20 23:31:31 -0400

    Fixed hr styling for both Homepage and Video Details page

--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -12,18 +12,18 @@ import "./Homepage.scss";
 const Homepage = () => {
     // Accessing the video context to use the shared state and functions across components
     const { videos, mainVideo, updateMainVideo } = useContext(VideoContext);
-
+    
     // Tracks the current URL path to react to navigation changes within the app
-    const location = useLocation();
+    const location = useLocation(); 
 
-    // Automatically select the first video as the main video when the component mounts or the videos list changes 
+    // Automatically select the first video as the main video when the component mounts or the videos list changes
     useEffect(() => {
         // Ensures there's at least one video to set as the main video
         if (videos.length > 0) {
             updateMainVideo(videos[0].id);
         }
-    }, [videos, updateMainVideo, location.pathname]); // Reacts to changes in the video list, updateMainVideo function, or the URL path 
-    
+    }, [videos, updateMainVideo, location.pathname]); // Reacts to changes in the videos list, updateMainVideo function, or the URL path
+
     return (
         <div className="homepage">
             {/* Conditionally render the main video and related components only if mainVideo is not null */}
@@ -33,7 +33,7 @@ const Homepage = () => {
                     <Hero mainVideo={mainVideo} />
                     <div className="homepage__video-info-container">
                         <div className="homepage__video-info-text">
-                            {/* VideoInfo component - diplays the title, description, and likes of the main video */}
+                            {/* VideoInfo component - displays the title, description, and likes of the main video */}
                             <VideoInfo mainVideo={mainVideo} />
                             {/* Comments component - lists comments for the main video */}
                             <Comments comments={mainVideo?.comments || []} />
--- src/pages/VideoDetailsPage/VideoDetailsPage.js
+++ src/pages/VideoDetailsPage/VideoDetailsPage.js
@@ -1,34 +1,34 @@
 import { useContext, useEffect } from "react";
 import { useParams } from "react-router-dom";
 
-// Importing context and child components for assembling the Video Details Page
-import { VideoContext } from "../../components/VideoPageManager/VideoPageManager";
+// Importing context and child components for assembling the Video Details page
+import { VideoContext } from "../../components/VideoPageManager/VideoPageManager"; 
 import Hero from "../../components/Hero/Hero";
 import VideoInfo from "../../components/VideoInfo/VideoInfo";
 import Comments from "../../components/Comments/Comments";
 import SideVideos from "../../components/SideVideos/SideVideos";
 
-// Imports specific styling to the Video Details page
-import "./VideoDetailsPage.scss"
+// Imports styling to the Video Details page
+import "./VideoDetailsPage.scss";
 
 const VideoDetailsPage = () => {
     // Retrieve the videoId from the URL
     const { videoId } = useParams();
     // Access context to use the video data and update function
-    const  { videos, mainVideo, updateMainVideo } = useContext(VideoContext);
+    const { videos, mainVideo, updateMainVideo } = useContext(VideoContext);
 
-    // Update main video based on URL change to videoId
+    // Updates main video based on URL change to videoId
     useEffect(() => {
         // Call updateMainVideo function if a videoId exists
         if (videoId) {
             updateMainVideo(videoId); // Fetch and display the details for the video corresponding to videoId
         }
     }, [videoId, updateMainVideo]); // Effect reruns when videoId changes or updateMainVideo function updates
-    
+
     return (
         <div className="videoDetailsPage">
-            {/* Conditionally rendering to ensure mainVideo data is available before attempting to display it  */}
-            { mainVideo && (
+            {/* Conditional rendering to ensure mainVideo data is available before attempting to display it */}
+            {mainVideo && ( 
                 <div className="videoDetailsPage__container">
                     {/* Display the selected video's title, description, and other info */}
                     <Hero mainVideo={mainVideo} />
